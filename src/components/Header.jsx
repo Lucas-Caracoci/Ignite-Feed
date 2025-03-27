88e91bcaf9e950c5b9381728d13390a499573083
Lucas Caracoci <lucascaracoci2@gmail.com>
2025-03-27 01:05:14 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './Header.module.css'
+import logo from '../assets/ignite-logo.svg'
 
 const Header = () => {
     return (
-       <header>
-        
+       <header className={styles.header}>
+            <img src={logo} alt="Logo ignite feed" />
+            <h2>Ignite Feed</h2>
        </header>
     );
 }
